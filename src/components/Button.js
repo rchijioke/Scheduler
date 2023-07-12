0b01060636dc057796ea6ec07a0be3bccc6b1cbf
Roy <ekeneroy@gmail.com>
2023-07-11 20:34:15 -0500

    Testing our Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,11 +9,13 @@ export default function Button(props) {
      buttonClass += " button--confirm";
    }
    if (props.danger) {
-      buttonClass += "button--danger";
+      buttonClass += " button--danger";
    }
-   return <button className={buttonClass}
+   return <button 
+   className={buttonClass}
    onClick={props.onClick}
    disabled={props.disabled}
-   >{props.children}</button>;
+   >
+   {props.children}</button>;
    
 }
