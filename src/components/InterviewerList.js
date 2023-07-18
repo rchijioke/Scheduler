850a9fc5fb15e7b46ffe4430e9e4181c98857a3d
Roy <ekeneroy@gmail.com>
2023-07-17 20:16:21 -0500

    Refactoring Interviewer Components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,21 +3,22 @@ import InterviewerListItem from "./InterviewerListItem";
 import "./InterviewerList.scss"
 
 export default function InterviewerList(props) {
-  const interviewerItems = props.interviewers.map((interviewer) => (
-    <InterviewerListItem
-      key={interviewer.id}
-      id={interviewer.id}
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      selected={props.interviewer === interviewer.id}
-      setInterviewer={props.setInterviewer}
-    />
-  ));
-
+  const interviewers = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
+      />
+    );
+  });
+  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{interviewerItems}</ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,22 +3,17 @@ import classNames from "classnames";
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
-  const interviewerClasses = classNames("interviewers__item", {
+  const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected,
   });
-
-  const handleItemClick = () => {
-    props.setInterviewer(props.id);
-  };
-
   return (
-    <li className={interviewerClasses} onClick={handleItemClick}>
-      <img
-        className="interviewers__item-image"
-        src={props.avatar}
-        alt={props.name}
-      />
-      {props.selected && props.name}
-    </li>
-  );
+    <li className={interviewerClass} onClick={props.setInterviewer}>
+    <img
+      className="interviewers__item-image"
+      src={props.avatar}
+      alt={props.name}
+    />
+    {props.selected && props.name}
+  </li>
+);
 }
