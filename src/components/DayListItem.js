f2b9dea3c730e5129c8b6c59e222649ceb9cb802
Roy <ekeneroy@gmail.com>
2023-08-15 21:07:05 -0500

    Navigating Days

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -17,10 +17,14 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
   });
-  return(
-    <li data-testid= "day" className={dayClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light"> {formatSpots(props.spots)} </h3>
-    </li>
-  )
+  return (
+  <li
+  className={dayClass}
+  onClick={() => props.setDay(props.name)}
+  data-testid="day" 
+>
+  <h2 className="text--regular">{props.name}</h2>
+  <h3 className="text--light">{formatSpots(props.spots)}</h3>
+</li>
+);
 }
